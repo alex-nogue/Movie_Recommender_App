de108c103604e2264764bbd3a4af76bbc0dad5c8
Alexandre Nogué Sanchez <49647860+alex-nogue@users.noreply.github.com>
2020-07-06 18:18:18 +0200

    added jaro-winkler distance

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import pandas as pd
 import json
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import textdistance as td
+
 app = Flask(__name__)
 
-data_api = pd.read_csv('data_api.csv')
+data_api = pd.read_csv('C:/Users/a.nogue.sanchez/OneDrive - Avanade/Documents/Perso/OpenClassrooms/OpenClassrooms_DataScience/Data_Science_V1_Projects/P3 - Developpez un moteur de recommandations de films/Results/data_api.csv')
 
 count = CountVectorizer(stop_words='english')
 count_matrix = count.fit_transform(data_api['soup'])
@@ -21,26 +23,27 @@ indices = indices.reset_index()
 def home():
     return render_template('index.html')
 
+
 #@app.route("/recommend/<string:wrd>")
 #def recommend(wrd):
-#    idx = int(indices[indices['movie_title'] == wrd][0])
-#    sim_scores = list(enumerate(cosine_sim[idx]))
-#    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-#    sim_scores = sim_scores[1:11]
-#    movie_indices = [i[0] for i in sim_scores]
-#    return json.dumps(list(data_api['movie_title'].iloc[movie_indices]))
-
-
-@app.route("/recommend",methods=['POST'])
+@app.route("/recommend", methods=['POST'])
 def recommend():
     wrd = str(request.form.get('word'))
+    message = "Displaying recommentations for " + wrd + ":\n"
+    if wrd not in indices['movie_title'].unique():
+        temp = indices
+        temp['movie_name_distance'] = indices.apply(lambda row: td.jaro_winkler(row['movie_title'], wrd), axis=1)
+        wrd = temp.sort_values('movie_name_distance', ascending=False).iloc[0,0]
+        message = "Couldn't find the input movie, displaying recommendations for " + wrd + ":<br/>"
     idx = int(indices[indices['movie_title'] == wrd][0])
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:11]
+    sim_scores = sim_scores[1:6]
     movie_indices = [i[0] for i in sim_scores]
-    return render_template('index.html', prediction='Recommended movies {}'.format(json.dumps(list(data_api['movie_title'].iloc[movie_indices]))))
-
+    recommendations = data_api['movie_title'].iloc[movie_indices].str.cat(sep='<br/>')
+    #return message + recommendations
+    #return render_template('index.html', prediction='Recommended movies {}'.format(json.dumps(message + recommendations)))
+    return render_template('index.html', prediction=message + recommendations)
 
 if __name__ == '__main__':
     app.run(debug=True)
